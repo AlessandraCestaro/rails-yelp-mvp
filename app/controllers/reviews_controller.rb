a1f8ada08dc91b14abadb4718e3d0e36528184cd
Alessandra Cestaro <alessandra_cestaro@mckinsey.com>
2018-10-24 23:59:20 +0200

    Rails App Yelp

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,33 @@
+class ReviewsController < ApplicationController
+before_action :set_review, only: [:show]
+
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.save
+
+    # Siamo sicuri di questo redirect?
+    # redirect_to reviews_path
+  end
+
+  private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,39 @@
+Restaurant.destroy_all
+p "Deleting Restaurant"
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '02784781310',
+    category:     'chinese',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '044918310',
+    category:     'italian'
+  },
+  {
+    name:         'Sushitop',
+    address:      'Via casamia 83, Milan',
+    phone_number: '09274920',
+    category:     'japanese'
+  },
+  {
+    name:         'Escargot',
+    address:      'Trogadero, Paris',
+    phone_number: '033565490',
+    category:     'french'
+  },
+  {
+    name:         'Pizza West',
+    address:      '56A Shoreditch High St, Singapore',
+    phone_number: '0011749184821',
+    category:     'italian'
+  },
+]
+Restaurant.create!(restaurants_attributes)
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
